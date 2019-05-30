b6adcbe5d2a4d9a9afaee2551b64bfc6a546a230
Paola <paola.obreguillermo@yahoo.com>
2019-05-30 16:46:10 -0500

    friends.js added

---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+var friends = [
+
+	{
+
+		name: "Selena Gomez",
+
+		photo: "../app/images/selena.png",
+
+		scores: [5, 1, 4, 3, 5, 2, 5, 2, 1, 3]
+
+	},
+
+	{
+
+		name: "Shawn Mendes",
+
+		photo: "../app/images/shawn.jpg",
+
+		scores: [2, 5, 4, 1, 3, 5, 4, 1, 3, 4]
+
+	},
+
+	{
+
+		name: "Charlie Puth",
+
+		photo: "../app/images/charlie.png",
+
+		scores: [5, 3, 1, 3, 3, 2, 1, 2, 3, 4]
+
+	},
+
+	{
+
+		name: "Zac Efron",
+
+		photo: "../app/images/zac.jpg",
+
+		scores: [3, 1, 3, 5, 3, 5, 2, 1, 5, 4]
+
+	},
+
+	{
+
+		name: "Lilly  Collins",
+
+		photo: "../app/images/lilly.jpg",
+
+		scores: [4, 2, 3, 4, 5, 5, 2, 5, 1, 1]
+
+    },
+    
+    {
+
+		name: "Demi Lovato",
+
+		photo: "../app/images/demi.png",
+
+		scores: [2, 1, 4, 1, 2, 4, 2, 5, 5, 4]
+
+	}
+
+
+];
+
+
+
+module.exports = friends;
