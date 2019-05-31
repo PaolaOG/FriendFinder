b1a847f581bc8761d3180f5579907e0024810a2f
Paola <paola.obreguillermo@yahoo.com>
2019-05-31 14:13:29 -0500

    added link

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,12 +4,12 @@ var friends = require("../data/friends.js");
 
 module.exports = function(app) {
    
-    app.get("/api/friends",function (req, res) {
+    app.get("../api/friends.js",function (req, res) {
         res.json(friends);
 
     });
 
-    app.post("api/friends", function (req, res) {
+    app.post("../api/friends.js", function (req, res) {
         console.log(req.body.scores);
     
 
